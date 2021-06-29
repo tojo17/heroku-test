6dca428030f758d918636fbe9bdcf9aebe059979
Sykie Chen <chen17@coder17.com>
2021-06-29 10:40:17 +0900

    mod: uvicorn start method

--- server.py
+++ server.py
@@ -1,25 +1,13 @@
 import sys
 import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 import hello as main
 
-origins = [
-    "*"
-]
 
-main.app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 if __name__ == "__main__":
     try:
         port = int(sys.argv[1])
     except:
         port = 8000
-    uvicorn.run(main.app, host="0.0.0.0", port=port)
+    uvicorn.run("hello:app", host="0.0.0.0", port=port, workers=1)
