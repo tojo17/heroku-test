9db2cb29401b5e5c73d44e6572a94640c1220b11
Sykie Chen <chen17@coder17.com>
2021-06-29 09:52:20 +0900

    fix: bugs

--- server.py
+++ server.py
@@ -4,13 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import hello as main
 
-app = FastAPI()
-
 origins = [
     "*"
 ]
 
-app.add_middleware(
+main.app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
@@ -19,4 +17,4 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(main.app, host="0.0.0.0", port=8000)
